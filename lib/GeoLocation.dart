9aff8658e82144f77b313ca6e4e0217b03ae0c6d
vinxentience <canlas.reyvincent@gmail.com>
2023-07-10 19:24:38 +0800

    google maps integration

--- lib/GeoLocation.dart
+++ lib/GeoLocation.dart
@@ -1,9 +1,15 @@
+import 'dart:async';
+
+import 'package:ebiseekleta_app/utils/location_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-Position? _currentPosition;
+double lat = 7.2539267;
+double long = 125.1207362;
 
 class GeoLocation extends StatefulWidget {
   const GeoLocation({super.key});
@@ -13,29 +19,91 @@ class GeoLocation extends StatefulWidget {
 }
 
 class _GeoLocationState extends State<GeoLocation> {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(lat, long), //default
+    zoom: 14.4746,
+  );
+
+  CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(lat, long),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  Marker _kLocationMarker = Marker(
+      icon: BitmapDescriptor.defaultMarker,
+      infoWindow: InfoWindow(title: 'You are here!'),
+      markerId: MarkerId('_kCurrentLoc'),
+      position: LatLng(lat, long));
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  initState() {
+    // TODO: implement initState
+    _kGooglePlex = CameraPosition(
+      target: LatLng(lat, long), //default
+      zoom: 5,
+    );
+    _getCurrentPosition();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('LAT: ${_currentPosition?.latitude ?? ""}'),
-          Text('LNG: ${_currentPosition?.longitude ?? ""}'),
-          const SizedBox(height: 32),
-          ElevatedButton(
-            onPressed: _getCurrentPosition,
-            child: const Text("Get Current Location"),
-          )
-        ],
+    return Scaffold(
+      body: GoogleMap(
+        zoomControlsEnabled: false,
+        markers: {_kLocationMarker},
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () async {
+          await _getCurrentPosition();
+          _goToTheLake();
+        },
+        label: const Text('Get Current Location'),
+        icon: const Icon(Icons.navigation),
       ),
     );
   }
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    //_determinePosition();
-    super.initState();
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+
+  Future<void> _getCurrentPosition() async {
+    final hasPermission = await _handleLocationPermission();
+    if (!hasPermission) return;
+    await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+        .then((Position position) {
+      setState(() {
+        _kLake = CameraPosition(
+            bearing: 192.8334901395799,
+            target: LatLng(position.latitude, position.longitude),
+            zoom: 19.151926040649414);
+        _kLocationMarker = Marker(
+            icon: BitmapDescriptor.defaultMarker,
+            infoWindow: InfoWindow(title: 'You are here!'),
+            markerId: MarkerId('_kCurrentLoc'),
+            position: LatLng(position.latitude, position.longitude));
+      });
+    }).catchError((e) {
+      debugPrint(e);
+    });
   }
 
   Future<bool> _handleLocationPermission() async {
@@ -66,15 +134,74 @@ class _GeoLocationState extends State<GeoLocation> {
     }
     return true;
   }
-
-  Future<void> _getCurrentPosition() async {
-    final hasPermission = await _handleLocationPermission();
-    if (!hasPermission) return;
-    await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
-        .then((Position position) {
-      setState(() => _currentPosition = position);
-    }).catchError((e) {
-      debugPrint(e);
-    });
-  }
 }
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Center(
+  //     child: Column(
+  //       mainAxisAlignment: MainAxisAlignment.center,
+  //       children: [
+  //         Text('LAT: ${_currentPosition?.latitude ?? ""}'),
+  //         Text('LNG: ${_currentPosition?.longitude ?? ""}'),
+  //         const SizedBox(height: 32),
+  //         ElevatedButton(
+  //           onPressed: _getCurrentPosition,
+  //           child: const Text("Get Current Location"),
+  //         )
+  //       ],
+  //     ),
+  //   );
+  // }
+
+  // @override
+  // void dispose() {
+  //   // TODO: implement dispose
+  //   super.dispose();
+  // }
+
+  // @override
+  // void initState() {
+  //   // TODO: implement initState
+  //   //_determinePosition();
+  //   super.initState();
+  // }
+
+  // Future<bool> _handleLocationPermission() async {
+  //   bool serviceEnabled;
+  //   LocationPermission permission;
+
+  //   serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  //   if (!serviceEnabled) {
+  //     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+  //         content: Text(
+  //             'Location services are disabled. Please enable the services')));
+  //     return false;
+  //   }
+  //   permission = await Geolocator.checkPermission();
+  //   if (permission == LocationPermission.denied) {
+  //     permission = await Geolocator.requestPermission();
+  //     if (permission == LocationPermission.denied) {
+  //       ScaffoldMessenger.of(context).showSnackBar(
+  //           const SnackBar(content: Text('Location permissions are denied')));
+  //       return false;
+  //     }
+  //   }
+  //   if (permission == LocationPermission.deniedForever) {
+  //     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+  //         content: Text(
+  //             'Location permissions are permanently denied, we cannot request permissions.')));
+  //     return false;
+  //   }
+  //   return true;
+  // }
+
+  // Future<void> _getCurrentPosition() async {
+  //   final hasPermission = await _handleLocationPermission();
+  //   if (!hasPermission) return;
+  //   await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+  //       .then((Position position) {
+  //     setState(() => _currentPosition = position);
+  //   }).catchError((e) {
+  //     debugPrint(e);
+  //   });
+  // }

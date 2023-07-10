752ce8975a18b8310b2b58f3041bd3ce5fc95910
vinxentience <canlas.reyvincent@gmail.com>
2023-07-10 19:26:02 +0800

    google maps integration

--- lib/GeoLocation.dart
+++ lib/GeoLocation.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:ebiseekleta_app/utils/location_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';

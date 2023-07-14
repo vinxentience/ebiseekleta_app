cbd1b32fa8570d74bdc45ce85f9e46e808370b43
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-07-14 18:10:23 +0800

    Format

--- lib/Homescreen.dart
+++ lib/Homescreen.dart
@@ -14,76 +14,77 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child:
-          Consumer<NetworkStatusProvider>(builder: (context, networkStatus, _) {
-        return Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/logo.png',
-              scale: 1.5,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            ListTile(
-              leading: (networkStatus.connectivityResult ==
-                          ConnectivityResult.wifi) ||
-                      (networkStatus.connectivityResult ==
-                          ConnectivityResult.mobile)
-                  ? const Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    )
-                  : const Icon(
-                      Icons.close,
-                      color: Colors.red,
-                    ),
-              title: Text(
-                  'Connection Status: ${networkStatus.connectivityResult}'),
-            ),
-            ListTile(
-              leading: networkStatus.wifiName != null
-                  ? const Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    )
-                  : const Icon(
-                      Icons.close,
-                      color: Colors.red,
-                    ),
-              title: Text(
-                  "Connected To: ${networkStatus.wifiName ?? 'WIFI or GPS is disabled'}"),
-            ),
-            ListTile(
-              leading: networkStatus.isGpsEnabled
-                  ? const Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    )
-                  : const Icon(
-                      Icons.close,
-                      color: Colors.red,
-                    ),
-              title: Text("GPS enabled: ${networkStatus.isGpsEnabled}"),
-            ),
-            ListTile(
-              leading: networkStatus.isInternetConnected
-                  ? const Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    )
-                  : const Icon(
-                      Icons.close,
-                      color: Colors.red,
-                    ),
-              title: Text(
-                  "Internet Connection: ${networkStatus.isInternetConnected}"),
-            ),
-          ],
-        );
-      }),
+      child: Consumer<NetworkStatusProvider>(
+        builder: (context, networkStatus, _) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/logo.png',
+                scale: 1.5,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              ListTile(
+                leading: (networkStatus.connectivityResult ==
+                            ConnectivityResult.wifi) ||
+                        (networkStatus.connectivityResult ==
+                            ConnectivityResult.mobile)
+                    ? const Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      )
+                    : const Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ),
+                title: Text(
+                    'Connection Status: ${networkStatus.connectivityResult}'),
+              ),
+              ListTile(
+                leading: networkStatus.wifiName != null
+                    ? const Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      )
+                    : const Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ),
+                title: Text(
+                    "Connected To: ${networkStatus.wifiName ?? 'WIFI or GPS is disabled'}"),
+              ),
+              ListTile(
+                leading: networkStatus.isGpsEnabled
+                    ? const Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      )
+                    : const Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ),
+                title: Text("GPS enabled: ${networkStatus.isGpsEnabled}"),
+              ),
+              ListTile(
+                leading: networkStatus.isInternetConnected
+                    ? const Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      )
+                    : const Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ),
+                title: Text(
+                    "Internet Connection: ${networkStatus.isInternetConnected}"),
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }

57b22847f1634580fb5b9b00073c570e5c5d3167
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-07-15 17:52:51 +0800

    Update permission_provider.dart

--- lib/permission_provider.dart
+++ lib/permission_provider.dart
@@ -2,25 +2,21 @@ import 'package:flutter/foundation.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class PermissionProvider extends ChangeNotifier {
-  bool _isLocationGranted = false;
-  bool _isSendSmsGranted = false;
+  PermissionStatus _location = PermissionStatus.denied;
+  PermissionStatus _sms = PermissionStatus.denied;
 
   PermissionProvider() {}
 
   void requestLocationPermission() async {
-    final status = await Permission.location.request();
-
-    _isLocationGranted = status.isGranted;
+    _location = await Permission.location.request();
     notifyListeners();
   }
 
   void requestSendSmsPermission() async {
-    final status = await Permission.sms.request();
-
-    _isSendSmsGranted = status.isGranted;
+    _sms = await Permission.sms.request();
     notifyListeners();
   }
 
-  bool get isLocationGranted => _isLocationGranted;
-  bool get isSendSmsGranted => _isSendSmsGranted;
+  PermissionStatus get location => _location;
+  PermissionStatus get sms => _sms;
 }

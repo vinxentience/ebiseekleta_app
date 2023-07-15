4098c91bcdd5a9b32640bd643a63bdea9f4aa88c
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-07-15 09:55:43 +0800

    Update gyro_provider.dart

--- lib/gyro_provider.dart
+++ lib/gyro_provider.dart
@@ -13,57 +13,81 @@ class GyroProvider extends ChangeNotifier {
   bool _isTilted = false;
   int _countdown = 10;
   Timer? _timer;
-  StreamController<bool> _needHelpController = StreamController.broadcast();
+  bool _exceededMaximumDuration = false;
+
+  StreamSubscription<AccelerometerEvent>? _accelerometerSub;
 
   GyroProvider() {
-    accelerometerEvents.listen((AccelerometerEvent event) {
-      _x = event.x;
-      _y = event.y;
-
-      final newIsTilted =
-          !((_x > 7 && _x < 11) && (_y > -3 && _y < 1) && (_z > -2 && _z < 2));
-
-      if (newIsTilted != _isTilted) notifyListeners();
-
-      _isTilted = newIsTilted;
-
-      if (_isTilted) {
-        print('gryo_provider: ${newIsTilted}');
-
-        if (_timer == null) {
-          print('timer started');
-          _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-            _countdown--;
-            notifyListeners();
-            if (_countdown == 0) {
-              print('timer stopped');
-              print('HELLLLLLPPPP');
-              _needHelpController.add(true);
-              _countdown = 10;
-              _timer?.cancel();
-              _timer = null;
-            }
-          });
-        } else {
-          print('timer already running');
-        }
-      }
-
-      if (!isTilted) {
-        print('timer stopped due to not tilted');
-        _countdown = 10;
-        notifyListeners();
-        _timer?.cancel();
-        _timer = null;
-      }
-
-      _needHelpController.add(false);
-    });
+    _accelerometerSub = accelerometerEvents.listen(_updateTiltStatus);
   }
 
   int get countdown => _countdown;
   bool get isTilted => _isTilted;
-  Stream<bool> get needHelpStream => _needHelpController.stream;
+  bool get exceededMaximumDuration => _exceededMaximumDuration;
+
+  void reset() {
+    _exceededMaximumDuration = false;
+    _countdown = 10;
+    _accelerometerSub = accelerometerEvents.listen(_updateTiltStatus);
+    notifyListeners();
+  }
+
+  void _updateTiltStatus(AccelerometerEvent event) {
+    _x = event.x;
+    _y = event.y;
+
+    final newIsTilted =
+        !((_x > 7 && _x < 11) && (_y > -3 && _y < 1) && (_z > -2 && _z < 2));
+
+    if (newIsTilted != _isTilted) {
+      notifyListeners();
+    }
+    _isTilted = newIsTilted;
+
+    if (_exceededMaximumDuration) {
+      return;
+    }
+
+    if (_isTilted) {
+      print('gryo_provider: ${newIsTilted}');
+
+      if (_timer == null) {
+        print('timer started');
+        _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+          _countdown--;
+          notifyListeners();
+          if (_countdown == 0) {
+            print('timer stopped');
+            print('HELLLLLLPPPP');
+            _countdown = 10;
+            _timer?.cancel();
+            _timer = null;
+
+            _exceededMaximumDuration = true;
+            _accelerometerSub!.cancel();
+            _accelerometerSub = null;
+          }
+        });
+      } else {
+        print('timer already running');
+      }
+    }
+
+    if (!isTilted) {
+      print('timer stopped due to not tilted');
+      _countdown = 10;
+      notifyListeners();
+      _timer?.cancel();
+      _timer = null;
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _accelerometerSub?.cancel();
+    _timer?.cancel();
+  }
 }
 
 

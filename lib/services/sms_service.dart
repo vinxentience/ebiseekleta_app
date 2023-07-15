c9e06d1d17973beb5ff37df26c7a3dd58691f425
Rey Vincent Canlas <canlas.reyvincent@gmail.com>
2023-07-15 13:12:56 +0800

    added cyclist chuchu

--- lib/services/sms_service.dart
+++ lib/services/sms_service.dart
@@ -1,22 +1,24 @@
 import 'package:background_sms/background_sms.dart';
 
 class SmsService {
+  final String cyclist;
   final List<String> recipients;
 
   SmsService({
+    required this.cyclist,
     required this.recipients,
-  });
+  }) : assert(recipients.isNotEmpty);
 
-  void send(String message, {String? location}) {
-    if (location != null) {
-      message += '\n\nLocation: $location';
-    }
+  void send({required String location}) {
+    String message = '$cyclist is in trouble need help! Location at: $location';
 
     for (var phoneNumber in recipients) {
       BackgroundSms.sendMessage(
         phoneNumber: phoneNumber,
         message: message,
-      );
+      ).then((value) {
+        print('message status $value. Recipient: $phoneNumber');
+      });
     }
   }
 }
